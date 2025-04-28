b31089c1ae40159503f276a87c039847dae760bf
NathanDalbert <dalbert.nathan@gmail.com>
2025-04-28 17:55:15 -0300

    feat: crair entidade usuario

--- wellspace/lib/models/Usuario.dart
+++ wellspace/lib/models/Usuario.dart
@@ -2,27 +2,28 @@ class Usuario {
   String nome;
   String email;
   String senha;
-  String confirmarSenha;
   DateTime dataNascimento;
   String fotoPerfil;
+  bool integridade; 
 
   Usuario({
     required this.nome,
     required this.email,
     required this.senha,
-    required this.confirmarSenha,
     required this.dataNascimento,
     required this.fotoPerfil,
+    this.integridade = false,
   });
+
 
   Map<String, dynamic> toJson() {
     return {
       'nome': nome,
       'email': email,
-      'senha': senha,
-      'confirmarSenha': confirmarSenha,
+      'senha': senha, 
       'dataNascimento': dataNascimento.toIso8601String(),
       'fotoPerfil': fotoPerfil,
+      'integridade': integridade,
     };
   }
 }
