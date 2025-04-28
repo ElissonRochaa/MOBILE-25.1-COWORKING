35dcdfac57cd765596a1e0c63b3ba1f0cbe7652e
NathanDalbert <dalbert.nathan@gmail.com>
2025-04-28 17:59:30 -0300

    refactor: ajustar visualização de senha na tela de login

--- wellspace/lib/views/LoginPage.dart
+++ wellspace/lib/views/LoginPage.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import '../services/UsuarioService.dart';  // Certifique-se de que o caminho está correto
-import '../config/JwtDecoder.dart';  // Importe o JwtDecoder corretamente
+import '../services/UsuarioService.dart';  
+import '../config/JwtDecoder.dart';  
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,55 +15,53 @@ class _LoginPageState extends State<LoginPage> {
   String _email = '';
   String _senha = '';
   bool _lembrarDeMim = false;
-  bool _obscureText = true; // Controla a visibilidade da senha
-
+  bool _obscureText = true; 
   // Função de login
   Future<void> _login() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       try {
-        // Exibe log de início de requisição
+       
         print('Iniciando requisição de login...');
-        
-        // Requisição de login para o backend
+    
         final response = await UsuarioService.login(_email, _senha);
 
-        // Exibe a resposta bruta do servidor
+       
         print('Resposta bruta do servidor: $response');
         
-        // Verifica se a resposta contém o token
+        
         if (response is String) {
-          // Salva o token JWT
+   
           String token = response;
           await UsuarioService.salvarToken(token);
 
-          // Decodifica o token
+        
           Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
           print('Decoded Token: $decodedToken');
 
-          // Exibe uma mensagem de sucesso
+        
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Login bem-sucedido!')),
           );
 
-          // Após o login bem-sucedido, você pode navegar para a próxima página
-          Navigator.pushReplacementNamed(context, '/home'); // Ou qualquer outra página que você tenha configurado
+        
+          Navigator.pushReplacementNamed(context, '/home');
         } else {
-          // Caso o token não esteja presente na resposta
+        
           print('Erro no login: Token não encontrado');
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Erro no login: Token não encontrado')),
           );
         }
       } catch (e) {
-        // Em caso de erro, exibe uma mensagem de erro
+       
         print('Erro durante o login: $e');
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Erro no login')),
         );
       }
     } else {
-      // Se o formulário não for válido
+     
       print('Erro: Formulário inválido');
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Por favor, preencha todos os campos')),
@@ -87,7 +85,7 @@ class _LoginPageState extends State<LoginPage> {
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 32),
-              // Campo de email
+             
               TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Email',
@@ -105,24 +103,24 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
               const SizedBox(height: 16),
-              // Campo de senha
+             
               TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Senha',
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
                     icon: Icon(
-                      // Muda o ícone conforme a visibilidade da senha
+                     
                       _obscureText ? Icons.visibility_off : Icons.visibility,
                     ),
                     onPressed: () {
                       setState(() {
-                        _obscureText = !_obscureText; // Alterna o estado de obscureText
+                        _obscureText = !_obscureText;
                       });
                     },
                   ),
                 ),
-                obscureText: _obscureText, // Controla a visibilidade da senha
+                obscureText: _obscureText, 
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira sua senha';
@@ -134,7 +132,7 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
               const SizedBox(height: 16),
-              // Checkbox "Lembrar de mim"
+             
               Row(
                 children: [
                   Checkbox(
@@ -149,7 +147,7 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
               const SizedBox(height: 24),
-              // Botão de login
+             
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
@@ -158,10 +156,10 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               const SizedBox(height: 16),
-              // Link para cadastro
+            
               GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, '/cadastro');  // Navega para a página de cadastro
+                  Navigator.pushNamed(context, '/cadastro');  
                 },
                 child: const Text(
                   'Não tem uma conta? Cadastre-se',
