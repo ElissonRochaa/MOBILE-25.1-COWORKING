34dbbdcc693ce25098eb085decc377230296eefb
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-11 23:12:20 -0300

    build(services): Aponta ChatService para a URL de produção

--- wellspace/lib/services/ChatService.dart
+++ wellspace/lib/services/ChatService.dart
@@ -3,7 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ChatService {
-  static const String _baseUrl = 'http://localhost:8443/api';
+  static const String _baseUrl = 'https://wellspace-app.onrender.com/api';
   static final _storage = FlutterSecureStorage();
 
   Future<String> sendMessage(String text) async {
