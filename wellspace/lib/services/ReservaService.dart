20dd8bee71354b077e7a7a0461faa1ff0ac3a6e5
Mickael-7 <mickael.albuquerque@upe.br>
2025-06-17 21:49:30 -0300

    fix(ReservaService): consertar Endpoints

--- wellspace/lib/services/ReservaService.dart
+++ wellspace/lib/services/ReservaService.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:Wellspace/models/Reserva.dart'; //
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
-import '../services/UsuarioService.dart'; 
+import '../services/UsuarioService.dart';
 
 class ReservaService {
   static const String _baseUrl = 'https://wellspace-app.onrender.com';
@@ -24,7 +24,7 @@ class ReservaService {
     try {
       final token = await _getTokenOrThrow();
       final response = await http.get(
-        Uri.parse('$_baseUrl/api/reservas/locatario/$usuarioId'), 
+        Uri.parse('$_baseUrl/api/reservas/locatario/$usuarioId'),
         headers: {
           'Authorization': 'Bearer $token',
         },
@@ -35,7 +35,7 @@ class ReservaService {
 
       if (_isSuccess(response.statusCode)) {
         final List<dynamic> data = json.decode(utf8.decode(response.bodyBytes));
-        return data.map((json) => Reserva.fromJson(json)).toList(); 
+        return data.map((json) => Reserva.fromJson(json)).toList();
       } else {
         print(
             '[ReservaService] Erro ao buscar reservas por usuário: ${response.body}');
@@ -47,23 +47,24 @@ class ReservaService {
     }
   }
 
-  static Future<Reserva?> criarReserva(ReservaRequest request) async { 
+  static Future<Reserva?> criarReserva(ReservaRequest request) async {
     try {
       final token = await _getTokenOrThrow();
       final response = await http.post(
-        Uri.parse('$_baseUrl/reservas'),
+        Uri.parse('$_baseUrl/api/reservas'),
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $token',
         },
-        body: json.encode(request.toJson()), 
+        body: json.encode(request.toJson()),
       );
 
       print('[ReservaService] criarReserva status: ${response.statusCode}');
 
       if (_isSuccess(response.statusCode)) {
         print('[ReservaService] Reserva criada com sucesso.');
-        return Reserva.fromJson(json.decode(utf8.decode(response.bodyBytes))); //
+        return Reserva.fromJson(
+            json.decode(utf8.decode(response.bodyBytes))); //
       } else {
         print('[ReservaService] Erro ao criar reserva: ${response.body}');
         return null;
@@ -78,7 +79,7 @@ class ReservaService {
     try {
       final token = await _getTokenOrThrow();
       final response = await http.delete(
-        Uri.parse('$_baseUrl/reservas/$reservaId'),
+        Uri.parse('$_baseUrl/api/reservas/$reservaId'),
         headers: {
           'Authorization': 'Bearer $token',
         },
@@ -92,15 +93,16 @@ class ReservaService {
     }
   }
 
-  static Future<List<Reserva>> listarReservasParaLocador(String locadorId) async {
-    final token = await UsuarioService.obterToken(); 
+  static Future<List<Reserva>> listarReservasParaLocador(
+      String usuarioId) async {
+    final token = await UsuarioService.obterToken();
     if (token == null) {
       throw Exception('Usuário não autenticado.');
     }
 
     try {
       final response = await http.get(
-        Uri.parse('$_baseUrl/reservas/locador/$locadorId'),
+        Uri.parse('$_baseUrl/api/reservas/locador/$usuarioId'),
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
           'Authorization': 'Bearer $token',
@@ -109,7 +111,7 @@ class ReservaService {
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(utf8.decode(response.bodyBytes));
-        return data.map((json) => Reserva.fromJson(json)).toList(); 
+        return data.map((json) => Reserva.fromJson(json)).toList();
       } else {
         throw Exception('Falha ao carregar reservas recebidas.');
       }
